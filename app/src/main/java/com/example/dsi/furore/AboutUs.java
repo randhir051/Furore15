718cad78d9e0a1864465b402ede943115f15fe76
Randhir Singh <rick.rox10@gmail.com>
2015-04-03 13:18:46 +0530

    UI fixes and gmail intent added

--- app/src/main/java/com/example/dsi/furore/AboutUs.java
+++ app/src/main/java/com/example/dsi/furore/AboutUs.java
@@ -1,15 +1,21 @@
 package com.example.dsi.furore;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.dsi.furore.R;
 
-public class AboutUs extends ActionBarActivity {
-
+public class AboutUs extends ActionBarActivity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +23,16 @@ public class AboutUs extends ActionBarActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
         toolbar.setTitle("About Us");
         setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        (findViewById(R.id.randhirmail)).setOnClickListener(this);
+        (findViewById(R.id.darshanmail)).setOnClickListener(this);
+        (findViewById(R.id.rohanmail)).setOnClickListener(this);
+        (findViewById(R.id.dikshamail)).setOnClickListener(this);
+        (findViewById(R.id.arjitmail)).setOnClickListener(this);
+        (findViewById(R.id.abhishekmail)).setOnClickListener(this);
+        (findViewById(R.id.antonymail)).setOnClickListener(this);
+
     }
 
 
@@ -38,7 +54,42 @@ public class AboutUs extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         }
+        if (id == R.id.home){
+            NavUtils.navigateUpFromSameTask(this);
+        }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        String email = null;
+        switch (id){
+            case R.id.randhirmail: email = getResources().getString(R.string.randhirmail);
+                break;
+            case R.id.darshanmail: email = getResources().getString(R.string.darshanmail);
+                break;
+            case R.id.rohanmail: email = getResources().getString(R.string.rohanmail);
+                break;
+            case R.id.dikshamail: email = getResources().getString(R.string.dikshamail);
+                break;
+            case R.id.abhishekmail: email = getResources().getString(R.string.abhishekmail);
+                break;
+            case R.id.arjitmail: email = getResources().getString(R.string.arjitmail);
+                break;
+            case R.id.antonymail: email = getResources().getString(R.string.antonymail);
+        }
+
+        if(email!=null){
+            Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+            sendIntent.setClassName("com.google.android.gm", "com.google.android.gm.ComposeActivityGmail");
+            sendIntent.putExtra(Intent.EXTRA_EMAIL, new String[] { email });
+            sendIntent.setData(Uri.parse(email));
+            sendIntent.putExtra(Intent.EXTRA_SUBJECT, "Furore app");
+            sendIntent.setType("plain/text");
+            sendIntent.putExtra(Intent.EXTRA_TEXT, "");
+            startActivity(sendIntent);
+        }
     }
 }
